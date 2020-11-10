3d9bdcaf61d034ecfccd3520eb9b14030f3c89f6
mohmmed-mahsoub <mohamedmahsoub181@gmail.com>
2020-11-10 22:12:28 +0200

    create ui and put data depends on api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,70 @@
 import "./App.css";
+import React, { Component } from "react";
 import Form from "./components/Form";
 import WeatherResults from "./components/WeatherResults";
-function App() {
-  return (
-    <div className="App">
-      <div className="app-cont">
-        <Form />
-        <WeatherResults />
+import axios from "axios";
+class App extends Component {
+  state = {
+    tempreature: "",
+
+    city: "",
+
+    country: "",
+
+    humidity: "",
+
+    description: "",
+
+    error: "",
+  };
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    if (city && country) {
+      const API_KEY = "73e180bfc84aef00782367406195238d";
+      const res = await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid5=${API_KEY}`
+      );
+      const dataWeather = res.data;
+      this.setState({
+        tempreature: dataWeather.main.temp,
+
+        city: dataWeather.name,
+
+        country: dataWeather.sys.country,
+
+        humidity: dataWeather.main.humidity,
+
+        description: dataWeather.weather[0].description,
+      });
+    } else {
+      this.setState({
+        tempreature: "",
+
+        city: "",
+
+        country: "",
+
+        humidity: "",
+
+        description: "",
+
+        error: "please fill field",
+      });
+    }
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="app-cont">
+          <Form getWeather={this.getWeather} />
+          <WeatherResults weatherData={this.state} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/WeatherResults.jsx
+++ src/components/WeatherResults.jsx
@@ -2,16 +2,40 @@ import React, { Component } from "react";
 class WeatherResults extends Component {
   state = {};
   render() {
+    const data = this.props.weatherData;
     return (
       <ul className="weather-results">
-        <li className="info">
-          <span className="info-key">temp:</span>
-          <span className="info-val">22</span>
-        </li>
-        <li className="info">
-          <span className="info-key">temp:</span>
-          <span className="info-val">22</span>
-        </li>
+        {data.tempreature && (
+          <li className="info">
+            <span className="info-key">tempreature:</span>
+            <span className="info-val">{data.tempreature}</span>
+          </li>
+        )}
+        {data.humidity && (
+          <li className="info">
+            <span className="info-key">humidity:</span>
+            <span className="info-val">{data.humidity}</span>
+          </li>
+        )}
+        {data.city && (
+          <li className="info">
+            <span className="info-key">city:</span>
+            <span className="info-val">{data.city}</span>
+          </li>
+        )}
+        {data.country && (
+          <li className="info">
+            <span className="info-key">country:</span>
+            <span className="info-val">{data.country}</span>
+          </li>
+        )}
+        {data.description && (
+          <li className="info">
+            <span className="info-key">description:</span>
+            <span className="info-val">{data.description}</span>
+          </li>
+        )}
+        {data.error && <li className="info">{data.error}</li>}
       </ul>
     );
   }
