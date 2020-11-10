35d3fcf8d2af7fd80427004193a69516e4a99137
mohmmed-mahsoub <mohamedmahsoub181@gmail.com>
2020-11-10 13:21:02 +0200

    create page UI as html/css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">start h</div>;
+  return (
+    <div className="App">
+      <div className="app-cont">
+        <form>
+          <input type="text" placeholder="city.." />
+          <input type="text" placeholder="country.." />
+          <button type="submit">get weather</button>
+        </form>
+        <div className="weather-results">
+          <ul>
+            <li className="info">
+              <span className="info-key">temp:</span>
+              <span className="info-val">22</span>
+            </li>
+            <li className="info">
+              <span className="info-key">temp:</span>
+              <span className="info-val">22</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
